157dd6b763b337d3a5652262726437055968a48e
Tusar <tusarahmed141@gmail.com>
2024-11-02 19:38:03 +0600

    Set and get local storage

--- Fundamentals/storage.js
+++ Fundamentals/storage.js
@@ -9,7 +9,7 @@
 // localStorage.setItem(name, 'hello');
 
 // const person = {
-//     name: 'Tusar', 
+//     name: 'Tusar',
 //     age: 25,
 //     major: 'Web',
 //     language: ['Bengoli', 'English', 'Hindi'],
@@ -20,21 +20,38 @@
 // const getData = localStorage.getItem('person');
 // console.log(JSON.parse(getData));
 
-
 // Set value to local storage
 const setValtoLocal = () => {
-    const inputId = document.getElementById('idInput');
-    const id = inputId.value;
-    const inputVal = document.getElementById('valInput');
-    const val = inputVal.value;
+  const inputId = document.getElementById("idInput");
+  const id = inputId.value;
+  const inputVal = document.getElementById("valInput");
+  const val = inputVal.value;
 
-    // Set To Local Storage
-    localStorage.setItem(id, val);
-}
+  // Set To Local Storage
+  localStorage.setItem(id, val);
+
+  // Empty after set
+  inputId.value = "";
+  inputVal.value = "";
+};
+
+document.getElementById("setValue").addEventListener("click", setValtoLocal);
+
+// Get Value From Local
+const getValFromLocal = () => {
+  for (const key in localStorage) {
+    if (Object.prototype.hasOwnProperty.call(localStorage, key)) {
+        const element = key;
+        console.log(element);
+    }
+  }
+};
+
+document.getElementById("getValue").addEventListener("click", getValFromLocal);
 
 // Clear value to local storage
 const clearLocal = () => {
-    return localStorage.clear();
-}
+  return localStorage.clear();
+};
 
-document.getElementById('clearValue').addEventListener('click', clearLocal);
+document.getElementById("clearValue").addEventListener("click", clearLocal);
