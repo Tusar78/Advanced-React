8dafa12976bc0f419d3e50b582c12057ea4be85d
Tusar <tusarahmed141@gmail.com>
2024-11-06 19:34:39 +0600

    Last update

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  const personInfo = {
+    name: 'Tusar',
+    age: 24,
+    job: 'Web Developer'
+  }
+  return (
+    <>
+      <Card personInfo={personInfo}></Card>
+    </>
+  );
+}
+
+function Card({personInfo:{name, age, job}}) {
+  const cardStyle = {
+    border: '1px solid palegreen',
+    borderRadius: '8px',
+    padding: '6px 60px'
+  }
 
   return (
     <>
-      <h1>Vite React</h1>
+      <div style={cardStyle}>
+        <h2>Name: {name}</h2>
+        <p>Age: {age}</p>
+        <p>Job: Web Developer</p>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
