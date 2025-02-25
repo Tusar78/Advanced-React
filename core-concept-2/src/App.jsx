e3254fcb52ed5283a26d0ef17f147d411cba67ad
Tusar <tusarahmed141@gmail.com>
2025-02-25 17:46:07 +0600

    Going to practice more example

--- core-concept-2/src/App.jsx
+++ core-concept-2/src/App.jsx
@@ -1,14 +1,11 @@
-import MountUnmount from "./Components/MountUnmount";
 import PracticeR from "./Components/PracticeR";
-import WidthEvent from "./Components/WidthEvent";
 
 const App = () => {
   return (
     <div className="grid grid-cols-2 gap-2 m-4">
       <PracticeR />
       <div>
-        <WidthEvent />
-        <MountUnmount />
+        
       </div>
     </div>
   );
